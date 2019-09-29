141ed270d291c359b3715be7d00764d5141b0db6
Alfian Losari <alfianlosari@gmail.com>
2019-09-29 16:21:04 +0700

    Add navigation between list and detail

--- Sources/TMDBMovieSwiftUIKit/MovieListView.swift
+++ Sources/TMDBMovieSwiftUIKit/MovieListView.swift
@@ -12,24 +12,32 @@ public struct MovieListView: View {
     
     @ObservedObject private var movieListViewModel: MovieListViewModel
     private let endpoint: MovieEndpoint
+    private let apiKey: String
     
     public init(apiKey: String, endpoint: MovieEndpoint) {
+        self.apiKey = apiKey
         self.movieListViewModel = MovieListViewModel(apiKey: apiKey)
         self.endpoint = endpoint
     }
     
     public var body: some View {
-        List(movieListViewModel.movies) { movie in
-            VStack {
-                Text(movie.title)
-                    .font(.subheadline)
-                Text(movie.overview)
-                    .font(.caption)
-                .lineLimit(2)
+        NavigationView {
+            List(movieListViewModel.movies) { movie in
+                NavigationLink(destination: MovieView(apiKey: self.apiKey, id: movie.id)) {
+                    VStack {
+                        Text(movie.title)
+                            .font(.subheadline)
+                        Text(movie.overview)
+                            .font(.caption)
+                            .lineLimit(2)
+                    }
+                }
+            }
+            .onAppear {
+                self.movieListViewModel.loadMovies(from: self.endpoint)
             }
         }
-        .onAppear {
-            self.movieListViewModel.loadMovies(from: self.endpoint)
-        }
+    
+        
     }
 }
