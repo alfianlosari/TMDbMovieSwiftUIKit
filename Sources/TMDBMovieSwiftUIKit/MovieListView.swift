0f9f2a2e4eb21d416128f49edf9c69f7192cfc93
Alfian Losari <alfianlosari@gmail.com>
2019-09-29 15:41:53 +0700

    Fix some issue on access modifier

--- Sources/TMDBMovieSwiftUIKit/MovieListView.swift
+++ Sources/TMDBMovieSwiftUIKit/MovieListView.swift
@@ -13,12 +13,12 @@ public struct MovieListView: View {
     @ObservedObject private var movieListViewModel: MovieListViewModel
     private let endpoint: MovieEndpoint
     
-    init(apiKey: String, endpoint: MovieEndpoint) {
+    public init(apiKey: String, endpoint: MovieEndpoint) {
         self.movieListViewModel = MovieListViewModel(apiKey: apiKey)
         self.endpoint = endpoint
     }
     
-    var body: some View {
+    public var body: some View {
         List(movieListViewModel.movies) { movie in
             Text(movie.title)
         }
