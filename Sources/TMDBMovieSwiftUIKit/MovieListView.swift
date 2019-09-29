46b84ff5326c187cb9509d28c40f665722b7cebb
Alfian Losari <alfianlosari@gmail.com>
2019-09-29 16:38:08 +0700

    Fix build error

--- Sources/TMDBMovieSwiftUIKit/MovieListView.swift
+++ Sources/TMDBMovieSwiftUIKit/MovieListView.swift
@@ -23,45 +23,53 @@ public struct MovieListView: View {
         self.endpoint = endpoint
     }
     
+    
+    #if os(watchOS)
+    
     public var body: some View {
-        #if os(watchOS)
-        return List(movieListViewModel.movies) { movie in
-            VStack {
-                Text(movie.title)
-                    .font(.subheadline)
-                Text(movie.overview)
-                    .font(.caption)
-                    .lineLimit(2)
-            }
-            .onTapGesture {
-                self.selectedId = movie.id
-                self.showingMovie.toggle()
-            }
-        }
-        .sheet(isPresented: $showingMovie) {
-            guard self.selectedId = selectedId else { return }
-            MovieView(apiKey: self.apiKey, id: selectedId)
-        }
-        .onAppear {
-            self.movieListViewModel.loadMovies(from: self.endpoint)
-        }
-        #else
-        return NavigationView {
-            List(movieListViewModel.movies) { movie in
-                NavigationLink(destination: MovieView(apiKey: self.apiKey, id: movie.id)) {
-                    VStack {
-                        Text(movie.title)
-                            .font(.subheadline)
-                        Text(movie.overview)
-                            .font(.caption)
-                            .lineLimit(2)
-                    }
-                }
-            }
-            .onAppear {
-                self.movieListViewModel.loadMovies(from: self.endpoint)
-            }
-        }
-        #endif
+           List(movieListViewModel.movies) { movie in
+               VStack {
+                   Text(movie.title)
+                       .font(.subheadline)
+                   Text(movie.overview)
+                       .font(.caption)
+                       .lineLimit(2)
+               }
+               .onTapGesture {
+                   self.selectedId = movie.id
+                   self.showingMovie.toggle()
+               }
+           }
+           .sheet(isPresented: $showingMovie) {
+               guard self.selectedId = selectedId else { return }
+               MovieView(apiKey: self.apiKey, id: selectedId)
+           }
+           .onAppear {
+               self.movieListViewModel.loadMovies(from: self.endpoint)
+           }
+    
     }
+    #else
+    public var body: some View {
+       NavigationView {
+           List(movieListViewModel.movies) { movie in
+               NavigationLink(destination: MovieView(apiKey: self.apiKey, id: movie.id)) {
+                   VStack {
+                       Text(movie.title)
+                           .font(.subheadline)
+                       Text(movie.overview)
+                           .font(.caption)
+                           .lineLimit(2)
+                   }
+               }
+           }
+           .onAppear {
+               self.movieListViewModel.loadMovies(from: self.endpoint)
+           }
+       }
+    }
+    #endif
+    
+ 
+    
 }
